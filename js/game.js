1378e1ee1e2fd716891585c101e610832f9afd9b
Ryan <tiangao0611@gmail.com>
2018-08-23 16:26:46 +0800

    finish Level constructor

--- js/game.js
+++ js/game.js
@@ -20,9 +20,23 @@ function Level(plan){
 		var line = plan[y], gridLine = [];
 		for(var x=0;x<this.width;x++){
 			var ch=line[x],fieldType=null;
-			var Actor = actorChars
+			var Actor = actorChars[ch];
+			if(Actor){
+				this.actors.push(new Actor(new Vector(x,y), ch));
+			}else if (ch == "x"){
+				fieldType = "wall";
+			}else if(ch == "!"){
+				fieldType = "lava";
+
+			}
+			gridLine.push(fieldType);
 		}
+		this.grid.push(gridLine);
 	}
+	this.player = this.actors.filter(function(actor){
+		return actor.type == "player";
+	})[0];
+	this.status = this.finishDelay = null;
 }
 function Vector(x,y){
 	this.x = x;
