50a89726563b033a76607f58101560f4697bc33e
Ryan <tiangao0611@gmail.com>
2018-08-24 16:42:07 +0800

    second edition

--- js/game.js
+++ js/game.js
@@ -10,136 +10,133 @@ var simpleLevelPlan = [
   "                      "
 ];
 
-function Level(plan){
-	this.width = plan[0].length;
-	this.height = plan.length;
-	this.grid = [];
-	this.actors = [];
+function Level(plan) {
+  this.width = plan[0].length;
+  this.height = plan.length;
+  this.grid = [];
+  this.actors = [];
 
-	for(var y=0;y<this.height;y++){
-		var line = plan[y], gridLine = [];
-		for(var x=0;x<this.width;x++){
-			var ch=line[x],fieldType=null;
-			var Actor = actorChars[ch];
-			if(Actor){
-				this.actors.push(new Actor(new Vector(x,y), ch));
-			}else if (ch == "x"){
-				fieldType = "wall";
-			}else if(ch == "!"){
-				fieldType = "lava";
+  for (var y = 0; y < this.height; y++) {
+    var line = plan[y], gridLine = [];
+    for (var x = 0; x < this.width; x++) {
+      var ch = line[x], fieldType = null;
+      var Actor = actorChars[ch];
+      if (Actor)
+        this.actors.push(new Actor(new Vector(x, y), ch));
+      else if (ch == "x")
+        fieldType = "wall";
+      else if (ch == "!")
+        fieldType = "lava";
+      gridLine.push(fieldType);
+    }
+    this.grid.push(gridLine);
+  }
 
-			}
-			gridLine.push(fieldType);
-		}
-		this.grid.push(gridLine);
-	}
-	this.player = this.actors.filter(function(actor){
-		return actor.type == "player";
-	})[0];
-	this.status = this.finishDelay = null;
+  this.player = this.actors.filter(function(actor) {
+    return actor.type == "player";
+  })[0];
+  this.status = this.finishDelay = null;
 }
-Level.prototype.isFinished = function(){
-	return this.status != null && this.finishDelay < 0;
+
+Level.prototype.isFinished = function() {
+  return this.status != null && this.finishDelay < 0;
 };
-function Vector(x,y){
-	this.x = x;
-	this.y = y;
+
+function Vector(x, y) {
+  this.x = x; this.y = y;
 }
-Vector.prototype.plus = function(other){
-    return new Vector(this.x+other.x, this.y+other.y);
+Vector.prototype.plus = function(other) {
+  return new Vector(this.x + other.x, this.y + other.y);
 };
-Vector.prototype.times = function(factor){
-	return new Vector(this.x*factor, this.y*factor);
+Vector.prototype.times = function(factor) {
+  return new Vector(this.x * factor, this.y * factor);
 };
 
 var actorChars = {
-	"@": Player,
-	"o": Coin,
-	"=": Lava,
-	"|": Lava,
-	"v": Lava
+  "@": Player,
+  "o": Coin,
+  "=": Lava, "|": Lava, "v": Lava
 };
 
-function Player(pos){
-	this.pos = pos.plus(new Vector(0, -0.5));
-	this.size = new Vector(0.8, 1.5);
-	this.speed = new Vector(0, 0);
+function Player(pos) {
+  this.pos = pos.plus(new Vector(0, -0.5));
+  this.size = new Vector(0.8, 1.5);
+  this.speed = new Vector(0, 0);
 }
+Player.prototype.type = "player";
 
-Player.prototype.type = "player"
-
-function Lava(pos, ch){
-	this.pos = pos;
-	this.size = new Vector(1,1);
-	if(ch == '='){
-		this.speed = new Vector(2, 0);
-	}else if (ch=="|"){
-		this.speed = new Vector(0,2);
-	}else if(ch=='v'){
-		this.speed = new Vector(0,3);
-		this.repeatPos = pos;
-	}
+function Lava(pos, ch) {
+  this.pos = pos;
+  this.size = new Vector(1, 1);
+  if (ch == "=") {
+    this.speed = new Vector(2, 0);
+  } else if (ch == "|") {
+    this.speed = new Vector(0, 2);
+  } else if (ch == "v") {
+    this.speed = new Vector(0, 3);
+    this.repeatPos = pos;
+  }
 }
+Lava.prototype.type = "lava";
 
-Lava.prototype.type = "Lava";
-
-function Coin(pos){
-	this.basePos = this.pos = pos.plus(new Vector(0.2, 0.1));
-	this.size = new Vector(0.6, 0.6);
-	this.wobble = Math.random()*Math.PI*2;
+function Coin(pos) {
+  this.basePos = this.pos = pos.plus(new Vector(0.2, 0.1));
+  this.size = new Vector(0.6, 0.6);
+  this.wobble = Math.random() * Math.PI * 2;
 }
 Coin.prototype.type = "coin";
 
-var simpleLevel = new Level(simpleLevelPlan)
-console.log(simpleLevel.width, 'by', simpleLevel.height);
+var simpleLevel = new Level(simpleLevelPlan);
 
-function elt(name, className){
-	var elt = document.createElement(name);
-	if (className) elt.className = className;
-	return elt;
+function elt(name, className) {
+  var elt = document.createElement(name);
+  if (className) elt.className = className;
+  return elt;
 }
 
-function DOMDisplay(parent, level){
-	this.wrap = parent.appendChild(elt("div", "game"));
-	this.level = level;
-	this.wrap.appendChild(this.drawBackground());
-	this.actorLayer = null;
-	this.drawFrame();
+function DOMDisplay(parent, level) {
+  this.wrap = parent.appendChild(elt("div", "game"));
+  this.level = level;
+
+  this.wrap.appendChild(this.drawBackground());
+  this.actorLayer = null;
+  this.drawFrame();
 }
 
 var scale = 20;
-DOMDisplay.prototype.drawBackground = function(){
-	var table = elt("table", "background");
-	table.style.width = this.level.width*scale + "px";
-	this.level.grid.forEach(function(row){
-		var rowElt = table.appendChild(elt("tr"));
-		rowElt.style.height = scale + "px";
-		row.forEach(function(type){
-			rowElt.appendChild(elt("td", type));
-		});
-	});
-	return table;
+
+DOMDisplay.prototype.drawBackground = function() {
+  var table = elt("table", "background");
+  table.style.width = this.level.width * scale + "px";
+  this.level.grid.forEach(function(row) {
+    var rowElt = table.appendChild(elt("tr"));
+    rowElt.style.height = scale + "px";
+    row.forEach(function(type) {
+      rowElt.appendChild(elt("td", type));
+    });
+  });
+  return table;
 };
 
-DOMDisplay.prototype.drawActors = function(){
-	var wrap = elt("div");
-	this.level.actors.forEach(function(actor){
-		var rect = wrap.appendChild(elt("div", "actor "+actor.type));
-		rect.style.width = actor.size.x * scale +"px";
-		rect.style.height = actor.size.y * scale +"px";
-		rect.style.left = actor.pos.x*scale + "px";
-		rect.style.top = actor.pox.y*scale + "px";
-	});
-	return wrap;
+DOMDisplay.prototype.drawActors = function() {
+  var wrap = elt("div");
+  this.level.actors.forEach(function(actor) {
+    var rect = wrap.appendChild(elt("div",
+                                    "actor " + actor.type));
+    rect.style.width = actor.size.x * scale + "px";
+    rect.style.height = actor.size.y * scale + "px";
+    rect.style.left = actor.pos.x * scale + "px";
+    rect.style.top = actor.pos.y * scale + "px";
+  });
+  return wrap;
 };
 
-DOMDisplay.prototype.drawFrame = function(){
-	if(this.actorLayer){
-		this.wrap.removeChild(this.actorLayer);
-	}
-	this.actorLayer = this.wrap.appendChild(this.drawActors());
-	this.wrap.className = "game" + (this.level.status || "");
-	this.scrollPlayerIntoView();
+DOMDisplay.prototype.drawFrame = function() {
+  if (this.actorLayer)
+    this.wrap.removeChild(this.actorLayer);
+  this.actorLayer = this.wrap.appendChild(this.drawActors());
+  this.wrap.className = "game " + (this.level.status || "");
+  this.scrollPlayerIntoView();
 };
 
 DOMDisplay.prototype.scrollPlayerIntoView = function() {
@@ -169,24 +166,22 @@ DOMDisplay.prototype.clear = function() {
   this.wrap.parentNode.removeChild(this.wrap);
 };
 
-Level.prototype.obstacleAt = function(){
-	var xStart = Math.floor(pos.x);
-	var xEnd = Math.ceil(pos.x + size.x);
-	var yStart = Math.floor(pos.y);
-	var yEnd = Math.ceil(pos.y+size.y);
+Level.prototype.obstacleAt = function(pos, size) {
+  var xStart = Math.floor(pos.x);
+  var xEnd = Math.ceil(pos.x + size.x);
+  var yStart = Math.floor(pos.y);
+  var yEnd = Math.ceil(pos.y + size.y);
 
-	if(xStart < 0 || xEnd > this.width || yStart < 0){
-		return "wall";
-	}
-	if(yEnd > this.height){
-		return "lava";
-	}
-	for(var y = yStart; y<yEnd;y++){
-		for(var x=xStart; x<xEnd;x++){
-			var fieldType = this.grid[y][x];
-			if (fieldType) return fieldType;
-		}
-	}
+  if (xStart < 0 || xEnd > this.width || yStart < 0)
+    return "wall";
+  if (yEnd > this.height)
+    return "lava";
+  for (var y = yStart; y < yEnd; y++) {
+    for (var x = xStart; x < xEnd; x++) {
+      var fieldType = this.grid[y][x];
+      if (fieldType) return fieldType;
+    }
+  }
 };
 
 Level.prototype.actorAt = function(actor) {
@@ -215,7 +210,6 @@ Level.prototype.animate = function(step, keys) {
     step -= thisStep;
   }
 };
-
 
 Lava.prototype.act = function(step, level) {
   var newPos = this.pos.plus(this.speed.times(step));
@@ -302,7 +296,6 @@ Level.prototype.playerTouched = function(type, actor) {
   }
 };
 
-
 var arrowCodes = {37: "left", 38: "up", 39: "right"};
 
 function trackKeys(codes) {
@@ -363,4 +356,3 @@ function runGame(plans, Display) {
   }
   startLevel(0);
 }
-
